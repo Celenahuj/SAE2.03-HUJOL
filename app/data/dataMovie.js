6bff509980aa2655c96c275543f4fa6dd95cbc2f
Celenahujol <celena.hujol@etu.unilim.fr>
2025-04-07 15:59:26 +0200

    iteration 4 update

--- app/data/dataMovie.js
+++ app/data/dataMovie.js
@@ -13,4 +13,26 @@ DataMovie.requestMovieDetails = async function (id) {
     let data = await response.json();
     return data;
 };
+
+DataMovie.requestCategories = async function () {
+    let answer = await fetch(HOST_URL + "/server/script.php?todo=getCategories");
+    let categories = await answer.json();
+    return categories;
+};
+
+DataMovie.requestCategories = async function () {
+    let answer = await fetch(HOST_URL + "/server/script.php?todo=getAllCategories");
+    let categories = await answer.json();
+    return categories;
+  };
+  
+  DataMovie.requestMoviecategorie = async function (categorie) {
+    let answer = await fetch(
+      HOST_URL +
+        "/server/script.php?todo=getMovieCategorie&category=" +
+        categorie
+    );
+    let movie = await answer.json();
+    return movie;
+  };
 export { DataMovie };
