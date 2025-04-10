d43dac269433c53f6d8cfc7917ec55d06dad4101
Celenahujol <celena.hujol@etu.unilim.fr>
2025-04-10 10:49:25 +0200

    iteration 9 bouton en coeur

--- app/data/dataMovie.js
+++ app/data/dataMovie.js
@@ -3,43 +3,43 @@ let HOST_URL = "https://mmi.unilim.fr/~hujol3/SAE2.03-HUJOL";
 let DataMovie = {};
 
 DataMovie.requestMovies = async function (age) {
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=readmovies&age="+age);
-    let data = await answer.json();
-    return data;
+  let answer = await fetch(HOST_URL + "/server/script.php?todo=readmovies&age=" + age);
+  let data = await answer.json();
+  return data;
 }
 
 
 DataMovie.requestMovieDetails = async function (id) {
-    let response = await fetch(HOST_URL + "/server/script.php?todo=details&id=" + id);
-    let data = await response.json();
-    console.log(data);
-    return data;
+  let response = await fetch(HOST_URL + "/server/script.php?todo=details&id=" + id);
+  let data = await response.json();
+  console.log(data);
+  return data;
 };
 
 DataMovie.requestCategories = async function () {
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=getCategories");
-    let categories = await answer.json();
-    return categories;
+  let answer = await fetch(HOST_URL + "/server/script.php?todo=getCategories");
+  let categories = await answer.json();
+  return categories;
 };
 
 DataMovie.requestCategories = async function () {
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=getAllCategories");
-    let categories = await answer.json();
-    return categories;
-  };
-  
-  DataMovie.requestMoviecategorie = async function (categorie, age=undefined) {
+  let answer = await fetch(HOST_URL + "/server/script.php?todo=getAllCategories");
+  let categories = await answer.json();
+  return categories;
+};
 
-    let params = "todo=getMovieCategorie&category=" + categorie;
+DataMovie.requestMoviecategorie = async function (categorie, age = undefined) {
 
-    if (age!=undefined)
-      params += "&age="+age;
+  let params = "todo=getMovieCategorie&category=" + categorie;
 
-    let answer = await fetch(
-      HOST_URL +
-        "/server/script.php?" + params
-    );
-    let movie = await answer.json();
-    return movie;
-  };
+  if (age != undefined)
+    params += "&age=" + age;
+
+  let answer = await fetch(
+    HOST_URL +
+    "/server/script.php?" + params
+  );
+  let movie = await answer.json();
+  return movie;
+};
 export { DataMovie };
